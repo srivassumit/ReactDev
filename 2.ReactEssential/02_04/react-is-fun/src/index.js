0f72c45468ce465f4c28d88fa7fc85020857738a
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-12 01:59:53 -0700

    code for 2.2.4

---
+++ 2.ReactEssential/02_04/react-is-fun/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'Arial'
+};
+
+ReactDOM.render(
+  <div style={style}>
+    <h1 id="heading">Hello World!</h1>
+    <p>We're glad you're here</p>
+  </div>,
+  document.getElementById('root')
+);
