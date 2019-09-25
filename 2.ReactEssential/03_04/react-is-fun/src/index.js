cfb665a4dc79b3bdc739799e30cfa8c02f8a1fed
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-25 00:08:00 -0700

    code for 2.3.4

---
+++ 2.ReactEssential/03_04/react-is-fun/src/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100
+};
+
+class SkiDayCounter extends Component {
+  getPercent = decimal => {
+    return decimal * 100 + '%';
+  };
+
+  calcGoalProgress = (total, goal) => {
+    return this.getPercent(total / goal);
+  };
+
+  render() {
+    const { total, powder, backcountry, goal } = this.props;
+    return (
+      <section>
+        <div>
+          <p>Total Days: {total}</p>
+        </div>
+        <div>
+          <p>Powder Days: {powder}</p>
+        </div>
+        <div>
+          <p>Backcountry Days: {backcountry}</p>
+        </div>
+        <div>
+          <p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
+        </div>
+      </section>
+    );
+  }
+}
+render(
+  // the number value should be within {} as a JSX expression
+  <SkiDayCounter
+    total={skiData.total}
+    powder={skiData.powder}
+    backcountry={skiData.backcountry}
+    goal={skiData.goal}
+  />,
+  document.getElementById('root')
+);
