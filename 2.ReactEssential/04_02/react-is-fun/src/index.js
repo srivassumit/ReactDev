fc4485d41be7239c7c934933b80a1226d4e2da80
Sumit Srivastava <ssrivas8@ncsu.edu>
2019-09-25 00:50:55 -0700

    code for 2.4.2

---
+++ 2.ReactEssential/04_02/react-is-fun/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+let bookList = [
+  { title: 'The Sun Also Rises', author: 'Ernest Hemingway', pages: 260 },
+  { title: 'White Teeth', author: 'Zadie Smith', pages: 480 },
+  { title: "Cat's Cradle", author: 'Kurt Vonnegut', pages: 304 },
+  { title: 'Hunger', author: 'Roxane Gay', pages: 320 }
+];
+
+const Book = ({ title, author, pages }) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <p>By: {author}</p>
+      <p>Pages: {pages} pages</p>
+    </section>
+  );
+};
+
+const Library = ({ books }) => {
+  return (
+    <div>
+      {books.map((book, i) => (
+        <Book
+          key={i}
+          title={book.title}
+          author={book.author}
+          pages={book.pages}
+        />
+      ))}
+    </div>
+  );
+};
+
+render(<Library books={bookList} />, document.getElementById('root'));
